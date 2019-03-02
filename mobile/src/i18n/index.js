6452dea76f77456eca7ade86d4ebc67530a2bf44
Adam Liechty <adam@doubledutch.me>
2019-03-01 16:54:09 -0800

    fix i18n paths

--- mobile/src/i18n/index.js
+++ mobile/src/i18n/index.js
@@ -1,12 +1,12 @@
-import enUS from './en-us/personal-leads.admin.json'
+import enUS from './en-us/personal-leads.mobile.json'
 
-import arINT from './ar-int/personal-leads.admin.json'
-import deDE from './de-de/personal-leads.admin.json'
-import esES from './es-es/personal-leads.admin.json'
-import frFR from './fr-fr/personal-leads.admin.json'
-import itIT from './it-it/personal-leads.admin.json'
-import jaJP from './ja-jp/personal-leads.admin.json'
-import nlNL from './nl-nl/personal-leads.admin.json'
+import arINT from './ar-int/personal-leads.mobile.json'
+import deDE from './de-de/personal-leads.mobile.json'
+import esES from './es-es/personal-leads.mobile.json'
+import frFR from './fr-fr/personal-leads.mobile.json'
+import itIT from './it-it/personal-leads.mobile.json'
+import jaJP from './ja-jp/personal-leads.mobile.json'
+import nlNL from './nl-nl/personal-leads.mobile.json'
 
 export default {
   '': enUS,
